940248a57b25efa04215e97ca75f9d0a2ae40522
Batuhan Aydin <114925623+batuhannaydin@users.noreply.github.com>
2026-01-12 12:51:52 +0300

    redisstore eklendi.

--- src/main/java/app/store/RedisStore.java
+++ src/main/java/app/store/RedisStore.java
@@ -1,25 +1,41 @@
-
 package app.store;
 
 import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
 import app.model.Student;
 import com.google.gson.Gson;
 
 public class RedisStore {
-    static Jedis jedis;
+    static JedisPool jedisPool;
     static Gson gson = new Gson();
 
     public static void init() {
-        jedis = new Jedis("localhost", 6379); // IP ve PORT burada
-        for (int i = 0; i < 10000; i++) {
-            String id = "2025" + String.format("%06d", i);
-            Student s = new Student(id, "Ad Soyad " + i, "Bilgisayar");
-            jedis.set(id, gson.toJson(s));
+        JedisPoolConfig poolConfig = new JedisPoolConfig();
+        poolConfig.setMaxTotal(60); 
+        jedisPool = new JedisPool(poolConfig, "localhost", 6379);
+        
+        // veri seti
+        String[] depts = {"AI Engineering", "Bioinformatics", "Data Science", "Cyber Security", "Robotics"};
+        String[] names = {"Caner", "Selin", "Bora", "İpek", "Kerem", "Deniz", "Ezgi", "Okan"};
+        String[] surnames = {"Öztürk", "Yılmaz", "Aksoy", "Erdemir", "Çetin", "Kaya", "Şen"};
+
+        try (Jedis jedis = jedisPool.getResource()) {
+            for (int i = 0; i < 10000; i++) {
+                String id = "2026" + String.format("%06d", i); 
+                String name = names[i % names.length] + " " + surnames[i % surnames.length];
+                Student s = new Student(id, name, depts[i % depts.length]);
+                
+                // Key formatı (student_no=xxxx)
+                jedis.set("student_no=" + id, gson.toJson(s));
+            }
         }
     }
 
-    public static Student get(String id) {
-        String json = jedis.get(id);
-        return gson.fromJson(json, Student.class);
+    public static Student get(String key) {
+        try (Jedis jedis = jedisPool.getResource()) {
+            String json = jedis.get(key);
+            return json != null ? gson.fromJson(json, Student.class) : null;
+        }
     }
 }
